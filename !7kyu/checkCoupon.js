957b2bfd7d1a43a3aa69057d9c708c1c70941312
Joel Huckle <joelhuckle2004@gmail.com>
2023-12-31 12:21:29 +0000

    check coupon

---
+++ !7kyu/checkCoupon.js
@@ -0,0 +1,4 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    const exp = new Date(expirationDate), cur = new Date(currentDate);  
+    return cur <= exp && enteredCode == correctCode;
+  }
